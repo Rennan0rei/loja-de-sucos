d25484f28243bae492e5eaf2e0b1ad26e882bec0
Rennan Rodrigues <rennan0rei@gmail.com>
2025-03-24 15:30:29 -0300

    LojaDeSucos_vue

---
+++ src/components/ListaDeSucos.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <SucoItem nome="Suco De Limão"
+            
+        />
+    </div>
+</template>
+
+<script>
+import SucoItem from './Sucoitem.vue';
+
+export default {
+    components: { SucoItem },
+    data() {
+        return { sucos: ["Laranja", "Maçã", "Uva"] };
+    },
+};
+</script>
---
+++ src/components/Sucoitem.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+
+<template>
+    <h1> Sucadas Maravilhas</h1>
+    <p>Suco de Limão</p>
+    <button>Comprar</button>
+    <p>Você comprou {{ contador }} vezes</p>
+</template>
+
+<script>
+export default {
+    props: ["nome"],
+    data() {
+        return { contador: 0};
+    },
+    methods: {
+        comprarSuco() {
+            this.contador++;
+        },
+    },
+};
+</script>
